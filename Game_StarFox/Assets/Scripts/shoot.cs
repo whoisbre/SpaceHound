6f8d1aa9ccfdb1d8616deb005a1175106641dc63
Fabian Kessler <bre_@hotmail.de>
2018-06-24 16:04:35 +0200

    shooting is now in a beta state -F

--- Game_StarFox/Assets/Scripts/shoot.cs
+++ Game_StarFox/Assets/Scripts/shoot.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections;
+﻿/*  shoot bullets if spacebar is pressed
+ * 
+ * Author: Fabian Kessler
+ */
+
+ /*
+  * andere key auf strg oder 0
+  */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,40 +16,61 @@ public class shoot : MonoBehaviour {
     // spawn point of bullet
     public GameObject BulletEmitter;
 
-    // ref das prefab
+    // ref prefab
     public GameObject Bullet;
 
     public float BulletSpeed;
     
-    // Use this for initialization
 	void Start () {
-		
+		 
 	}
-	
-	void Update () {
-        // press space to shoot
-        if (Input.GetKeyDown("space")) {
-            
+
+    float fireRate = 255f;
+    float nextTimeToFire = 0f;
+
+    void Update () {
+
+        // shoot if "spacebar" or "numpad 0" or "numpad enter" is pressed
+        if (Input.GetKey("space") && Time.time >= nextTimeToFire ||
+            Input.GetKey("[0]") && Time.time >= nextTimeToFire || 
+            Input.GetKey("enter") && Time.time >= nextTimeToFire) {
+
+            // short interval till next shot
+            nextTimeToFire = Time.time + (1f / fireRate);
+
             // create bullet
-        GameObject TempBulletHandler;
+            GameObject TempBulletHandler;
             TempBulletHandler = Instantiate(Bullet,
                 BulletEmitter.transform.position,
                 BulletEmitter.transform.rotation)
                 as GameObject;
 
-                // maybe
-                // TempBulletHandler.transform.Rotate(Vector3.left * 90);
-            
             Rigidbody TempRigidBody;
             TempRigidBody = TempBulletHandler.GetComponent<Rigidbody>();
-
+                
             // push bullet forward
-            TempRigidBody.AddForce(transform.forward * BulletSpeed * 222);
+            TempRigidBody.AddForce(transform.forward * BulletSpeed * 11); // war 222
 
             Destroy(TempBulletHandler, 2.0f);
-        
-        // failstate?
-    // spaceHound
         }
-	}
+    }
 }
+                // maybe
+                // TempBulletHandler.transform.Rotate(Vector3.left * 90);
+    //public bool doShoot = false;
+    //public int doShoot;
+/*	   public int shootcnt = 0;
+    int doShoot = 0;
+        // shoot every second frame
+        shootcnt = shootcnt++;
+        if (shootcnt >= 6) { shootcnt = 0; }
+        Debug.Log(shootcnt);
+*/
+/*  Changelog:
+ *      24 Jun  added multiple keys,
+ *              you can shoot holding down space,
+ *              fixed position in y axis
+ *      23 Jun  rigidbody constr - freeze rotation 
+ *              
+ * 
+ */
