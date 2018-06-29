f0937676308fad43d0ecb88f83100ee56a41d837
ziontrane <lukas.heitkamp@informatik.hs-fulda.de>
2018-06-29 20:57:25 +0200

    Added PlanetSpawner and PlanetMovement

---
+++ SpaceHoundBuildL/Assets/Scripts/PlanetMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetMovement : MonoBehaviour
+{
+
+    private Rigidbody planetrig;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    planetrig = GetComponent<Rigidbody>();
+	    planetrig.AddForce(new Vector3(0, -10, -10), ForceMode.VelocityChange);
+    }
+	
+}
---
+++ SpaceHoundBuildL/Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    private float timer = 2f;
+    private GameObject planet;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    planet = GameObject.Find("PlanetOrigin");
+	    planet.name = "Planet";
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    timer -= Time.deltaTime;
+	    
+
+        if (timer <= 0f)
+	    {
+	        Instantiate(planet, new Vector3(Random.Range(-9f, 9), 11, Random.Range(10, 30)),
+	            Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+            
+	        timer = 2f;
+	    }
+
+
+	}
+
+
+}
