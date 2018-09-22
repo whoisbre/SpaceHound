0ddee67c47181a0a26482707667cf60389eddb55
whoisbre <bre_@hotmail.de>
2018-09-22 10:19:24 +0200

    edit Lights

---
+++ SpaceHoundBuildL/Assets/Scripts/FlashScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 100000; i++) { }
+        /*
+         * GameObject Flash;
+            Flash = GameObject.Find("Flash");
+            //.GetComponent<Renderer>().material = LightOFF;
+            //LightObjects[i].GetChild(0).GetComponent<Light>().enabled = false;
+            Light lightComp = Flash.AddComponent<Light>();
+            lightComp.color = Color.blue;
+            Flash.transform.position = new Vector3(0, 5, 0);
+            Destroy(this);
+         */
+ 
+            }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
